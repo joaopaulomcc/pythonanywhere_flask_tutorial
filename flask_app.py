a38f09fd48be1d16cab8f8a95bb93cc4d9ecf32b
joaopaulomcc <joaopaulomcc@gmail.com>
2022-02-20 18:11:22 +0000

    Add SQLAlchemy setup code.

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,23 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="joaopaulomcc",
+    password="123zxc456asd",
+    hostname="joaopaulomcc.mysql.pythonanywhere-services.com",
+    databasename="joaopaulomcc$comments",
+)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
